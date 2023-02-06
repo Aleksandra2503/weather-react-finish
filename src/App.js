45f034b60e8f69dff945ca9a3e5a18392de26137
Aleksandra2503 <113549101+Aleksandra2503@users.noreply.github.com>
2023-02-07 00:02:43 +0100

    Hello

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
         <p>
           !Edit <code>src/App.js</code> and save to reload.
         </p>
+        <h2>HELLO</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
