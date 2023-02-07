4a97eb260117f8de29323398bcf548f63ef53273
Aleksandra2503 <113549101+Aleksandra2503@users.noreply.github.com>
2023-02-07 20:13:21 +0100

    2

---
+++ src/API.js
@@ -0,0 +1,4 @@
+export default class Api {
+    static url = "https://api.openweathermap.org";
+    static key = "6f2c8d8e771e0ede0a15520e397b527c";
+}
--- src/App.js
+++ src/App.js
@@ -1,30 +1,15 @@
-import React from "react";
-import Weather from "./weather";
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import Navigation from "./Navigation";
+import Main from "./Main";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Weather />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          !!!Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h2>HELLO</h2>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Weather city="Murnau"/>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navigation />
+        <Main />
+      </div>
+    );
+  }
 }
-
-export default App;
